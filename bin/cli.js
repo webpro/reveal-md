582451b68d7566d11e171a491263e1289deca880
Lars Kappert <lars@webpro.nl>
2018-03-14 00:37:58 +0100

    Rewrite

--- bin/cli.js
+++ bin/cli.js
@@ -1,50 +1,30 @@
 #!/usr/bin/env node
 
 'use strict';
-const path = require('path'),
-  program = require('commander'),
-  debug = require('debug')('reveal-md'),
-  _ = require('lodash'),
-  libDefaults = require('../lib/defaults'),
-  revealMarkdown = require('./../lib'),
-  pkg = require('../package.json');
-
-let localOptions = {};
-try {
-  localOptions = require(path.join(process.cwd(), 'reveal-md.json'));
-} catch(err) {
-  debug(err.message);
-}
-debug('localOptions %O', localOptions);
-
-let revealOptions = {};
-try {
-  revealOptions = require(path.join(process.cwd(), 'reveal.json'));
-} catch(err) {
-  debug(err.message);
-}
-debug('revealOptions %O', revealOptions);
-
-const defaults = _.extend({}, libDefaults, localOptions);
+const path = require('path');
+const program = require('commander');
+const defaults = require('../lib/defaults');
+const revealMarkdown = require('./../lib');
+const pkg = require('../package.json');
 
 program
   .version(pkg.version)
   .usage('<slides.md> [options]')
   .option('    --disable-auto-open', 'Disable auto-opening your web browser', defaults.disableAutoOpen)
-  .option('    --highlight-theme <theme>', `Highlight theme [${libDefaults.highlightTheme}]`, defaults.highlightTheme)
-  .option('    --host <host>', `Host [${libDefaults.host}]`, defaults.host)
+  .option('    --highlight-theme <theme>', `Highlight theme [${defaults.highlightTheme}]`, defaults.highlightTheme)
+  .option('    --host <host>', `Host [${defaults.host}]`, defaults.host)
   .option('    --scripts <files>', 'Scripts to inject into the page', defaults.scripts)
   .option('    --css <files>', 'CSS files to inject into the page', defaults.css)
   .option('    --preprocessor <script>', 'Markdown preprocessor script', defaults.preprocessor)
-  .option('    --port <port>', `Port [${libDefaults.port}]`, defaults.port)
+  .option('    --port <port>', `Port [${defaults.port}]`, defaults.port)
   .option('    --print [filename]', 'Print', defaults.print)
-  .option('-t, --theme <theme>', `Theme [${libDefaults.theme}]`)
+  .option('-t, --theme <theme>', `Theme [${defaults.theme}]`)
   .option('    --title <title>', 'Title of the presentation')
   .option('-s, --separator <separator>', 'Slide separator')
   .option('    --static [dir]', 'Export static html to directory [_static]. Incompatible with --print.', defaults.static)
   .option('    --static-dirs <dirs>', 'Extra directories to copy into static directory. Only used in conjunction with --static.', defaults.staticDirs)
   .option('-S, --vertical-separator <separator>', 'Vertical slide separator')
-  .option('-w, --watch', `Watch for changes in markdown file and livereload presentation [${libDefaults.watch}]`, defaults.watch)
+  .option('-w, --watch', `Watch for changes in markdown file and livereload presentation [${defaults.watch}]`, defaults.watch)
   .option('    --template <filename>', 'Template file for reveal.js', defaults.template)
   .option('    --listing-template <filename>', 'Template file for listing', defaults.listingTemplate)
   .parse(process.argv);
@@ -52,7 +32,5 @@ program
 if(program.args.length > 2) {
   program.help();
 }
-
-program.revealOptions = revealOptions;
 
 revealMarkdown(program);
--- test/render.spec.js
+++ test/render.spec.js
@@ -6,8 +6,8 @@ const markdown = fs.readFileSync('demo/a.md').toString();
 
 describe('render', () => {
 
-  it('should render basic template', (done) => {
-    render.render('', {}).then((actual) => {
+  it('should render basic template', () => {
+    return render.render('', {}).then(actual => {
       expect(actual).toContain('<title>reveal-md</title>');
       expect(actual).toContain('<link rel="stylesheet" href="/css/theme/black.css"');
       expect(actual).toContain('<link rel="stylesheet" href="/css/highlight/zenburn.css"');
@@ -16,27 +16,24 @@ describe('render', () => {
       expect(actual).toContain('<script src="/js/reveal.js"></script>');
       expect(actual).toContain('{ src: \'/plugin/markdown/markdown.js\'');
       expect(actual).toContain('var options = {};');
-      done();
     });
   });
 
-  it('should render markdown content', (done) => {
-    render.render('# header', {}).then((actual) => {
+  it('should render markdown content', () => {
+    return render.render('# header', {}).then(actual => {
       expect(actual).toContain('<div class="slides"><section  data-markdown><script type="text/template"># header</script></section></div>');
-      done();
     });
   });
 
-  it('should render custom scripts', (done) => {
-    render.render('# header', { scripts: 'custom.js,also.js' }).then((actual) => {
+  it('should render custom scripts', () => {
+    return render.render('# header', { scripts: 'custom.js,also.js' }).then(actual => {
       expect(actual).toContain('<script src="/_assets/custom.js"></script>');
       expect(actual).toContain('<script src="/_assets/also.js"></script>');
-      done();
     });
   });
 
-  it('should render custom css after theme', (done) => {
-    render.render('# header', { css: 'style1.css,style2.css' }).then((actual) => {
+  it('should render custom css after theme', () => {
+    return render.render('# header', { css: 'style1.css,style2.css' }).then(actual => {
       const themeLink = '<link rel="stylesheet" href="/css/highlight/zenburn.css">';
       const style1Link = '<link rel="stylesheet" href="/_assets/style1.css">';
       const style2Link = '<link rel="stylesheet" href="/_assets/style2.css">';
@@ -45,107 +42,104 @@ describe('render', () => {
       expect(actual).toContain(style2Link);
       expect(actual.indexOf(style1Link)).toBeGreaterThan(actual.indexOf(themeLink));
       expect(actual.indexOf(style2Link)).toBeGreaterThan(actual.indexOf(style1Link));
-      done();
     });
   });
 
-  it('should render print stylesheet', (done) => {
-    render.render('', { print: true }).then((actual) => {
+  it('should render print stylesheet', () => {
+    return render.render('', { print: true }).then(actual => {
       expect(actual).toContain('<link rel="stylesheet" href="/css/print/pdf.css" type="text/css" media="print">');
-      done();
     });
   });
 
-  it('should render alternate theme stylesheet', (done) => {
-    render.render('', { theme: 'white' }).then((actual) => {
+  it('should render alternate theme stylesheet', () => {
+    return render.render('', { theme: 'white' }).then(actual => {
       expect(actual).toContain('<link rel="stylesheet" href="/css/theme/white.css"');
-      done();
     });
   });
 
-  it('should render root-based domain-less links for static markup', (done) => {
-    render.render('', { static: true }).then((actual) => {
+  it('should render root-based domain-less links for static markup', () => {
+    return render.render('', { static: true }).then(actual => {
       expect(actual.match(/href="\.\//g).length).toBe(4);
       expect(actual.match(/src="\.\//g).length).toBe(2);
       expect(actual.match(/src:\ '\.\//g).length).toBe(7);
-      done();
     });
   });
 
-  it('should render reveal.js options', (done) => {
-    render.render('', { revealOptions: { controls: false } }).then((actual) => {
+  it('should render reveal.js options', () => {
+    return render.render('', { revealOptions: { controls: false } }).then(actual => {
       expect(actual).toContain('var options = {"controls":false};');
-      done();
     });
   });
+
+  it('should render title from YAML front matter', () => {
+    return render.render('---\ntitle: Foo Bar\n---\nSlide', {}).then(actual => {
+      console.log(actual);
+      expect(actual).toMatch(/<title>Foo Bar<\/title>/);
+    });
+  });
+
+
 });
 
-describe('parseSlides', (done) => {
+describe('parseSlides', () => {
 
-  it('should render slides split by horizontal separator', (done) => {
-    render.parseSlides('Slide A\n\n---\n\nSlide B', {}).then((actual) => {
+  it('should render slides split by horizontal separator', () => {
+    render.parseSlides('Slide A\n\n---\n\nSlide B', {}).then(actual => {
       expect(actual.slides).toEqual('' +
         '<section  data-markdown><script type="text/template">Slide A\n</script></section>' +
         '<section  data-markdown><script type="text/template">\nSlide B</script></section>');
-      done();
     });
   });
 
-  it('should render sub slides split by vertical separator', (done) => {
-    render.parseSlides('Slide A\n\n---\n\nSlide B\n\n----\n\nSlide C', {}).then((actual) => {
+  it('should render sub slides split by vertical separator', () => {
+    render.parseSlides('Slide A\n\n---\n\nSlide B\n\n----\n\nSlide C', {}).then(actual => {
       expect(actual.slides).toEqual('' +
         '<section  data-markdown><script type="text/template">Slide A\n</script></section>' +
         '<section >' +
         '<section data-markdown><script type="text/template">\nSlide B\n</script></section>' +
         '<section data-markdown><script type="text/template">\nSlide C</script></section>' +
         '</section>');
-      done();
     });
   });
 
-  it('should render slides split by custom separators', (done) => {
-    render.parseSlides('Slide A\n\n\nSlide B\n\nSlide C', { separator: '\n\n\n', verticalSeparator: '\n\n' }).then((actual) => {
+  it('should render slides split by custom separators', () => {
+    render.parseSlides('Slide A\n\n\nSlide B\n\nSlide C', { separator: '\n\n\n', verticalSeparator: '\n\n' }).then(actual => {
       expect(actual.slides).toEqual('' +
         '<section  data-markdown><script type="text/template">Slide A</script></section>' +
         '<section >' +
         '<section data-markdown><script type="text/template">Slide B</script></section>' +
         '<section data-markdown><script type="text/template">Slide C</script></section>' +
         '</section>');
-      done();
     });
   });
 
-  it('should render speaker notes', (done) => {
-    render.parseSlides('Slide A\n\nNote: test', {}).then((actual) => {
+  it('should render speaker notes', () => {
+    render.parseSlides('Slide A\n\nNote: test', {}).then(actual => {
       expect(actual.slides).toEqual('<section  data-markdown><script type="text/template">Slide A\n\n<aside class="notes"><p>test</p>\n</aside></script></section>');
-      done();
     });
   });
 
-  it('should parse YAML front matter', (done) => {
-    render.parseSlides('---\nseparator: <!--s-->\n---\nSlide A<!--s-->Slide B', {}).then((actual) => {
+  it('should parse YAML front matter', () => {
+    render.parseSlides('---\nseparator: <!--s-->\n---\nSlide A<!--s-->Slide B', {}).then(actual => {
       expect(actual.slides).toEqual('' +
         '<section  data-markdown><script type="text/template">\nSlide A</script></section>' +
         '<section  data-markdown><script type="text/template">Slide B</script></section>');
-      done();
     });
   });
 
-  it('should ignore comments (e.g. custom slide attributes)', (done) => {
-    render.parseSlides('Slide A\n\n---\n\n<!-- .slide: data-background="./image1.png" -->\nSlide B', {}).then((actual) => {
+  it('should ignore comments (e.g. custom slide attributes)', () => {
+    render.parseSlides('Slide A\n\n---\n\n<!-- .slide: data-background="./image1.png" -->\nSlide B', {}).then(actual => {
       expect(actual.slides).toEqual('' +
         '<section  data-markdown><script type="text/template">Slide A\n</script></section>' +
         '<section  data-markdown><script type="text/template">\n<!-- .slide: data-background="./image1.png" -->\nSlide B</script></section>');
-      done();
     });
   });
 
-  it('should use preprocesser for markdown', (done) => {
-    render.parseSlides('# Slide A\n\ncontent\n\n# Slide B\n\ncontent', { preprocessor: 'test/preproc' }).then((actual) => {
+  it('should use preprocesser for markdown', () => {
+    render.parseSlides('# Slide A\n\ncontent\n\n# Slide B\n\ncontent', { preprocessor: 'test/preproc' }).then(actual => {
       expect(actual.slides).toEqual('' +
         '<section  data-markdown><script type="text/template"># Slide A\n\ncontent\n\n</script></section>' +
         '<section  data-markdown><script type="text/template">\n# Slide B\n\ncontent</script></section>');
-      done();
     });
   });
 
