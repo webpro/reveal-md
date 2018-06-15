f42cd42d73c8cff1ddb51edfd805894207973329
Juhamatti Niemelä <iiska@iki.fi>
2018-06-15 22:31:38 +0300

    Option for Puppeteer launch args
    Allow customizing how Chromium is launched. This is needed to workaround
    Chromium sandbox issues when tasks requiring Puppeteer are run in CI
    setups.
    
    See for more information:
    https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md#chrome-headless-fails-due-to-sandbox-issues

--- bin/cli.js
+++ bin/cli.js
@@ -42,6 +42,7 @@ program
     '    --absolute-url <url>',
     'Define url used for hosting static build. This is included in OpenGraph metadata. Only used with --static.'
   )
+  .option('    --puppeteer-launch-args <args>', 'Customize how Puppeteer launches Chromium. Needed for some CI setups.')
   .parse(process.argv);
 
 if (program.args.length > 2) {
