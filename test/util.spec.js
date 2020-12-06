86f9ddacee86bbfd60f68a4a3857ac4642279099
Lars Kappert <lars@webpro.nl>
2020-12-06 13:40:25 +0100

    Rename `listFiles` helper to `getFilePaths`

--- test/util.spec.js
+++ test/util.spec.js
@@ -1,19 +1,19 @@
 const test = require('bron');
 const assert = require('assert').strict;
 const { join } = require('path');
-const { listFiles } = require('../lib/util');
+const { getFilePaths } = require('../lib/util');
 const { getFilesGlob } = require('../lib/config');
 
 const FIXTURES_DIR = join(__dirname, 'fixtures');
 
 test('should list all Markdown files with default config', async () => {
   const expected = ['a.md', 'slides.md', 'sub/c.md', 'sub/slides.md'];
-  const actual = listFiles(FIXTURES_DIR, getFilesGlob());
+  const actual = getFilePaths(FIXTURES_DIR, getFilesGlob());
   assert.deepEqual(expected, actual);
 });
 
 test('should list only files matching the glob pattern', async () => {
   const expected = ['slides.md', 'sub/slides.md'];
-  const actual = listFiles(FIXTURES_DIR, '**/slides.md');
+  const actual = getFilePaths(FIXTURES_DIR, '**/slides.md');
   assert.deepEqual(expected, actual);
 });
